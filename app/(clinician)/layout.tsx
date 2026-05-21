758cdfae36fca870406965e4705c3cef5fd37611
mihretc <mihretdesalegn@cheche.et>
2026-05-21 11:35:29 +0300

    fix: build error

--- app/(clinician)/layout.tsx
+++ app/(clinician)/layout.tsx
@@ -1,6 +1,7 @@
 import Sidebar from "./_components/sidebar";
 import  Footer  from "@/components/footer";
 import { ClinicianHeader } from "./_components/clinician-header";
+import TopHeader from "../_components/TopHeader";
 
 export default function ClinicianLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -8,7 +9,8 @@ export default function ClinicianLayout({ children }: { children: React.ReactNod
       <div className="flex flex-1 min-h-0">
         <Sidebar />
         <div className="flex-1 flex flex-col min-w-0">
-          <TopHeader />
+          {/* <TopHeader /> */}
+          <ClinicianHeader />
           {children}
         </div>
       </div>
